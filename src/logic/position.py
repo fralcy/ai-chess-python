b316809164a04c14db2db34a4dd7661a31c3e40b
Quang Thinh Le <quangthinh2924@gmail.com>
2025-03-19 13:25:05 +0700

    add position class

---
+++ src/logic/position.py
@@ -0,0 +1,30 @@
+class position():
+  def __init__(self, row, column):
+    self.row = row
+    self.column = column
+
+  @property
+  def row(self):
+    return self._row
+  
+  @property
+  def column(self):
+    return self._column
+  
+  def square_color(self):
+    """Return the color of the square."""
+    from logic.player import Player
+    if (self.row + self.column) % 2 == 0:
+      return Player.WHITE
+    else:
+      return Player.BLACK
+  
+  def __eq__(self, other):
+    return self.row == other.row and self.column == other.column
+  
+  def __hash__(self):
+    """Return the hash of the position."""
+    return hash((self.row, self.column))
+  
+  def __ne__(self, value):
+    return not self == value
